bea10ba50cc2884c4f1545b4dc2845eed97ca86d
MadiBentley <mb.madisonbentley@gmail.com>
2017-08-06 16:32:08 -0700

    made colour changing work for one colour but not a longer pattern

--- simon/script.js
+++ simon/script.js
@@ -2,7 +2,19 @@
 var pattern = [];
 var init = function(){
   generatePattern();
+  displayPattern(pattern);
   console.log(pattern);
+}
+var displayPattern = function (arr){
+  for (var x = 0; x < arr.length; x++){
+    changeColour(arr[x]);
+  }
+}
+var changeColour = function(colour){
+  $("#" + colour).css("background-color", "black");
+  window.setTimeout(function(){
+    $("#" + colour).css("background-color", colour);
+  }, 600);
 }
 var generatePattern = function(){
   pattern.push(addToPattern());
