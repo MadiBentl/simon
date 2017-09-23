3a9cc6774e0bd8d3c1484db12b0d3970b183f3dc
MadiBentley <mb.madisonbentley@gmail.com>
2017-09-23 13:28:32 -0700

    Start cycling through ddata

--- hackathon2017/scripts/scripts.js
+++ hackathon2017/scripts/scripts.js
@@ -1,4 +1,13 @@
 console.log("successful");
 $(document).ready(function(){
   $("body").append("<h1>test</h1>");
+  var dataLength = 3;
+  for (var x = 0; x < dataLength; x++){
+    $("#options")
+    .append("<div class='option' id='option" + x + "'></div>");
+    $("#option" + x)
+    .append("<h3>Moto</h3>")
+    .append("<p>Price:" + x + "</p>")
+    .append("<a href='google.com'>book now</a>")
+  }
 });
