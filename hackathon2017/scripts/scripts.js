cea4cdcc1e1daba37fd1d1ccb01e9c5115db05ca
MadiBentley <mb.madisonbentley@gmail.com>
2017-09-23 13:55:43 -0700

    Add functionality to form

--- hackathon2017/scripts/scripts.js
+++ hackathon2017/scripts/scripts.js
@@ -1,13 +1,19 @@
 console.log("successful");
 $(document).ready(function(){
   var dataLength = 3;
-  for (var x = 0; x < dataLength; x++){
-    $("#options")
-    .append("<div class='option' id='option" + x + "'></div>");
-    $("#option" + x)
-    .append("<h3>Moto</h3>")
-    .append("<p>Price:" + x + "</p>")
-    .append("<p>Time:" + x + " minutes</p>")
-    .append("<a href='google.com'>book now</a>");
+  $( "#target" ).submit(function( event ) {
+    displayOptions();
+    event.preventDefault();
+  });
+  var displayOptions = function(){
+    for (var x = 0; x < dataLength; x++){
+      $("#options")
+      .append("<div class='option' id='option" + x + "'></div>");
+      $("#option" + x)
+      .append("<h3>Moto</h3>")
+      .append("<p>Price:" + x + "</p>")
+      .append("<p>Time:" + x + " minutes</p>")
+      .append("<a href='google.com'>book now</a>");
+    }
   }
 });
