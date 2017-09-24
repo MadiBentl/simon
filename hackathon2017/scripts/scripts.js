89864465aac390fa349d15d03e10887b3f43167f
MadiBentley <mb.madisonbentley@gmail.com>
2017-09-23 22:58:00 -0700

    made sample ride share options appear

--- hackathon2017/scripts/scripts.js
+++ hackathon2017/scripts/scripts.js
@@ -17,10 +17,21 @@ $(document).ready(function(){
 
 });
 var displayOptions = function(){
-//  $(document).ready(function(){
     if (addressEntered){
       $("#address-options").addClass("hidden");
       $("#car-options").removeClass("hidden");
+      createOptions();
     }
-  //});
+}
+var createOptions = function(){
+  for (var x = 0; x< dataLength; x++){
+    $("#car-options").append("<div class='option' id='option" + x +"'>");
+    $("#option" + x).append("<div class='fltright' id='fltright" + x +"'></div>")
+                    .append("<div class='fltlft' id='fltleft" + x + "''></div>");
+    $("#fltright" + x).append("<h3> \uD83D\uDEB6 > \uD83D\uDE97 Moto > \uD83D\uDEB6</h3>")
+    .append("</br>")
+    .append("Most Peaceful");
+    $("#fltleft" + x).append("<p>Cost: $20</p>")
+                    .append("<p> 5 minutes</p>");
+  }
 }
